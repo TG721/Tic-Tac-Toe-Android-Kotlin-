cbec0d0d32b6f9c0307615073e5749db7a17d6b7
tengiz <tengiz152@gmail.com>
2023-01-11 20:04:07 +0400

    game is playable

--- app/src/main/java/com/tengizmkcorp/tic_tac_toe/ui/element/GameFragment.kt
+++ app/src/main/java/com/tengizmkcorp/tic_tac_toe/ui/element/GameFragment.kt
@@ -12,6 +12,8 @@ import com.tengizmkcorp.tic_tac_toe.ui.model.VALUES
 class GameFragment : BaseFragment<FragmentGameBinding>(FragmentGameBinding::inflate) {
     private lateinit var cellItemAdapter: CellItemAdapter
     private var cellList = mutableListOf<Cell>()
+    private var numberOfOccupiedCells = 0
+    private var gameOver = false
     override fun setup() {
         setupRecycler()
     }
@@ -20,13 +22,30 @@ class GameFragment : BaseFragment<FragmentGameBinding>(FragmentGameBinding::infl
         for (i in 0..8) {
             cellList.add(Cell(xVisibility = false, oVisibility = false, value = VALUES.NONE))
         }
-        cellItemAdapter = CellItemAdapter(cellList)
+        cellItemAdapter = CellItemAdapter(cellList) { item, pos ->
+            placeTheMove(item, pos)
+        }
         val recycler = binding.RVgame
         recycler.layoutManager =
             GridLayoutManager(requireContext(), 3, LinearLayoutManager.VERTICAL, false)
         recycler.adapter = cellItemAdapter
         recycler.addItemDecoration(ItemOffsetDecoration(0))
 
+    }
+
+    private fun placeTheMove(item: Cell, pos: Int) {
+        if (item.value == VALUES.NONE) {
+            if (numberOfOccupiedCells % 2 == 0) {
+                cellList[pos].xVisibility = true
+                cellList[pos].value = VALUES.X
+            }
+            else {
+                cellList[pos].oVisibility = true
+                cellList[pos].value = VALUES.O
+            }
+            cellItemAdapter.notifyDataSetChanged()
+            numberOfOccupiedCells++
+        }
     }
 
     override fun listeners() {
